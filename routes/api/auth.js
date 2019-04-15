1ac2dfc0eb891a673389614717f7f105cd5c381b
kiminohero <bishal5058prime@gmail.com>
2019-04-16 00:19:12 +0530

    debug google authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,7 +13,7 @@ module.exports = app => {
       "/auth/google/callback",
       passport.authenticate("google"),
       (req, res) => {
-        res.redirect("/");
+        res.redirect("/todos");
       }
     );
   } catch (e) {
