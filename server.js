f3c31aa5a4e6b277c98e8a341fb2f600bc44c0a3
kiminohero <bishal5058prime@gmail.com>
2019-04-16 00:01:29 +0530

    deploy debug server.js

--- server.js
+++ server.js
@@ -36,5 +36,14 @@ require("./routes/api/todos")(app);
 //   });
 // });
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port);
